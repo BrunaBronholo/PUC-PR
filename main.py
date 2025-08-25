edb2f07463587cd5ad3a7f52bf7b810133e5313f
Bruna Bronholo <homanoffmusic@gmail.com>
2025-08-24 22:47:02 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+erros_comuns = [
+    "Achar que '==' e '=' são a mesma coisa",
+    "Fazer um loop infinito e não saber como parar",
+    "Instalar a biblioteca errada e culpar o Python",
+    "Copiar código do StackOverflow sem entender nada",
+    "Salvar o arquivo, mas rodar a versão antiga sem perceber",
+]
+
+print("Lista de erros que todo jovem programador vai cometer:")
+print("e ta tudo bem!")
+print(random.choice(erros_comuns))
